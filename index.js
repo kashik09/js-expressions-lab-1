2989760aa2ab716263212b4933173a85a38c567d
kashik09 <kashiku789@gmail.com>
2025-11-16 17:24:15 +0300

    Group daily temperatures into Fahrenheit and Celsius arrays

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+const tempsF = [
+  day1TempF,
+  day3TempF,
+  day5TempF,
+  day7TempF,
+  day9TempF,
+  day11TempF,
+  day13TempF,
+  day15TempF,
+  day17TempF,
+  day19TempF,
+  day21TempF,
+  day23TempF,
+  day25TempF,
+  day27TempF,
+  day29TempF,
+];
 
-
-//! Start the calculation of the total temperatures
-//* Then apply the conversion to calculate the total in the other unit of measurement
-//* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
-
-//! Start the calculation of the average temperatures
-//* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
-
-//! Console.log the results for your own inspection if you'd like
-
-//! After creating the four variables mentioned above, uncomment the following lines
-//* This way you can export them to the test file, this is essential for the tests to work
-
-module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
-};
+const tempsC = [
+  day2TempC,
+  day4TempC,
+  day6TempC,
+  day8TempC,
+  day10TempC,
+  day12TempC,
+  day14TempC,
+  day16TempC,
+  day18TempC,
+  day20TempC,
+  day22TempC,
+  day24TempC,
+  day26TempC,
+  day28TempC,
+  day30TempC,
+];
